af841fd68e4a5e50e5ab40d942c7c18adf5ed6f9
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-02-17 17:51:31 -0500

    Add controller tests for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/users", to: "users#index"
   get "/users/:id", to: "users#show"
   get "/users/:id/posts", to: "posts#index"
   get "/users/:id/posts/:id", to: "posts#show"
---
+++ spec/requests/post_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /users/1/posts' do
+    before(:example) { get '/users/1/posts' }
+    it 'should work!' do
+      expect(response).to have_http_status(200)
+    end
+    it 'renders template correctly' do
+      expect(response).to render_template(:index)
+    end
+    it "doesn\'t renders show template  other than /users" do
+      expect(response).to_not render_template(:show)
+    end
+    it 'shows the correct placeholder text in index' do
+      expect(response.body).to include('<p>Find me in app/views/posts/index.html.erb</p>')
+    end
+  end
+  describe 'GET /users/:id/posts/:id' do
+    before(:example) { get '/users/1/posts/1' }
+    it 'should work!' do
+      expect(response).to have_http_status(200)
+    end
+    it 'shows the correct placeholder text in show' do
+      expect(response.body).to include('<p>Find me in app/views/posts/show.html.erb</p>')
+    end
+    it 'renders show template correctly' do
+      expect(response).to render_template(:show)
+    end
+    it "doesn\'t renders index template  other than /users#show" do
+      expect(response).to_not render_template(:index)
+    end
+  end
+end
