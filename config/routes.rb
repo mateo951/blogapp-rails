43f4486d61bdca0e596f007c74969deaad27612a
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-02-17 10:35:14 -0500

    Add controllers, actions and routes for users and posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
+  root "users#index" 
 end
