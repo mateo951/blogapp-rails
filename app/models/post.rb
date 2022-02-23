fb77fe543f871e58a6bf7d56ddaa62e53c26d8f4
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-02-22 23:25:47 -0500

    Add custom methods for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,14 @@ class Post < ApplicationRecord
     belongs_to :author_id
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
+    
+    A method which returns the 5 most recent comments for a given post.
+
+    def update_posts_counter()
+        user.update(posts_counter: user.posts_counter + 1)
+    end
+
+    def recent_comments()
+        posts.limit(3)
+    end
 end
