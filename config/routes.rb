ff2d3d25cf8884d6f9e96d7192f77d604d9bc415
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-02-25 12:21:10 -0500

    Add form for like creation

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,11 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/:post_id", to: "posts#show"
   get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
   get '/users/:user_id/posts/:post_id', to: 'comments#new', as: 'new_comment' # new
-  
+  get '/users/:user_id/posts/:post_id/new_like', to: 'likes#new', as: 'new_like' # new
+
   post '/users/:id/posts/new', to: 'posts#create' # create
   post '/users/:user_id/posts/:post_id', to: 'comments#create' # create
+  post '/users/:user_id/posts/:post_id/new_like', to: 'likes#create' # create
 
   root "users#index" 
 end
