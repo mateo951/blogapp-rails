3665902c8f583232a4d64b5f6515208747979884
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-02-25 12:13:49 -0500

    Add form for comment creation

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts", to: "posts#index"
   get "/users/:user_id/posts/:post_id", to: "posts#show"
   get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
+  get '/users/:user_id/posts/:post_id', to: 'comments#new', as: 'new_comment' # new
   
   post '/users/:id/posts/new', to: 'posts#create' # create
+  post '/users/:user_id/posts/:post_id', to: 'comments#create' # create
 
   root "users#index" 
 end
 
-# get '/users/:user_id/posts/:post_id', to: 'comments#new', as: 'new_comment' # new
+
 # get '/users/:user_id/posts/:post_id/new_like', to: 'likes#new', as: 'new_like' # new
