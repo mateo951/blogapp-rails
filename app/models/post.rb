7b6fd0f63a7474079dda892c166bb6fe6d3800d5
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-02-25 20:55:34 -0500

    Add validations into models

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+
   def self.update_counter(id)
     @user = User.find(id)
     @user.update(posts_counter: (@user.posts_counter + 1))
