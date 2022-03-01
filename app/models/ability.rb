5c047e545b533f1c79b07cd188a024560ea77275
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-01 13:48:42 -0500

    Add syntax fix for ability model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,17 +2,13 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    include CanCan::Ability
+    user ||= User.new # guest user (not logged in)
+    can :read, :all
 
-    def initialize(user)
-      user ||= User.new # guest user (not logged in)
-      can :read, :all
-  
-      return unless user.present?
-  
-      can :read, :all
-      can :manage, Post, author_id: user.id
-      can :manage, Comment, author_id: user.id
-    end
+    return unless user.present?
+
+    can :read, :all
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
   end
 end
