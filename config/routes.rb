6c4fcd189f8a6b9fd534fe93f97aa8e033b87a19
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-01 22:22:26 -0500

    Add hotfix for post deletion

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/users/:user_id/posts/:post_id/destroy_comment', to: 'comments#destroy', as: 'destroy_comment' # destroy
   get '/users/:user_id/posts/:post_id/destroy_post', to: 'posts#destroy', as: 'destroy_post' # destroy
 
+
   get '/users/:id/posts/new', to: 'posts#new', as: 'new_post' # new
   get '/users/:user_id/posts/:post_id/new_like', to: 'likes#new', as: 'new_like' # new
   get '/users/:user_id/posts/:post_id/comment', to: 'comments#new', as: 'new_comment' # new
@@ -13,6 +14,7 @@ Rails.application.routes.draw do
   get "/users/:user_id/posts/:post_id", to: "posts#show"
   get "/users/:user_id/posts", to: "posts#index"
   
+  post '/users/:user_id/posts/:post_id/destroy_post', to: 'posts#destroy' # destroy
   post '/users/:user_id/posts/:post_id/destroy_comment', to: 'comments#destroy' # destroy
   post '/users/:id/posts/new', to: 'posts#create' # create
   post '/users/:user_id/posts/:post_id/comment', to: 'comments#create' # create
