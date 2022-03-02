41e5c0895b0f2e52402e04d93a60120e5cd63f1d
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-02 16:02:19 -0500

    Add fix for bio not showing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter email password password_confirmation bio])
   end
 end
