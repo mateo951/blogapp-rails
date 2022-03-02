6d6ebb05cf25847cef36f31204565111f0b6b23a
Mateo Villagómez <mateovillagomez1995@gmail.com>
2022-03-02 15:48:07 -0500

    Revert "Add fix for bio not showing"
    This reverts commit 345fae62e3c062399a8a56c5428507501fb65deb.

--- Gemfile
+++ Gemfile
@@ -37,7 +37,6 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rails-controller-testing'
   gem 'rspec-rails', '~> 5.0.0'
-  gem "database_cleaner"
 end
 
 group :development do
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter email password password_confirmation bio])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name posts_counter email password password_confirmation])
   end
 end
