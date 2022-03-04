34b0e6c8fe49a743859663e29736533642e1881d
AlexRS90 <aramosbengy@gmail.com>
2022-03-04 16:29:45 -0600

    Fix Layout/EndOfLine: Carriage return character detected

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :jwt_authenticatable, jwt_revocation_strategy: self
-         
+
   has_many :posts, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: 'author_id', dependent: :destroy
   has_many :likes, foreign_key: 'author_id', dependent: :destroy
